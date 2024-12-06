183de8d40ba5b6d2594266f4140ed03af9fd82fd
edsjourney1 <edsjourney1@gmail.com>
2024-12-06 10:03:13 +0530

    changes for nephrocare

--- blocks/cards/cards.js
+++ blocks/cards/cards.js
@@ -14,5 +14,30 @@ export default function decorate(block) {
   });
   ul.querySelectorAll('picture > img').forEach((img) => img.closest('picture').replaceWith(createOptimizedPicture(img.src, img.alt, false, [{ width: '750' }])));
   block.textContent = '';
+  
   block.append(ul);
+  movePictureInsideAnchor();
+}
+
+function movePictureInsideAnchor() {
+  // Select all the 'li' elements inside the 'cards' block
+  const listItems = document.querySelectorAll('.cards.block ul li');
+
+  listItems.forEach((li) => {
+    // Find the 'cards-card-image' div within the current 'li' element
+    const imageDiv = li.querySelector('.cards-card-image');
+
+    if (imageDiv) {
+      // Find the 'picture' and 'a' elements inside the 'cards-card-image' div
+      const pictureTag = imageDiv.querySelector('picture');
+      const anchorTag = imageDiv.querySelector('a');
+
+      if (pictureTag && anchorTag) {
+        // Move the 'picture' tag inside the 'a' tag
+        anchorTag.innerHTML = ''; // Clear the anchor tag content
+        anchorTag.appendChild(pictureTag); // Append the picture tag inside the anchor tag
+        imageDiv.append(anchorTag, imageDiv.firstChild); // Ensure the anchor tag is the first element
+      }
+    }
+  });
 }
